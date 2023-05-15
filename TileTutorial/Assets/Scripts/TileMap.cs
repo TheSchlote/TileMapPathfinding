1470278a365d94c19f3fd6a57af0557e2d2c6454
TheSchlote <TheSchlote@gmail.com>
2023-05-14 22:24:39 -0500

    Civilization/Dungeon Tile Movement & Pathfinding #3
    unit moves!

--- TileTutorial/Assets/Scripts/TileMap.cs
+++ TileTutorial/Assets/Scripts/TileMap.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class TileMap : MonoBehaviour
 {
-    
+    public GameObject selectedUnit;
     public TileType[] tileTypes;
 
     int[,] tiles;
@@ -12,14 +12,28 @@ public class TileMap : MonoBehaviour
     int mapSizeY = 10;
     private void Start()
     {
+        GenerateMapData();
+        GenerateMapVisual();
+    }
+
+    private void GenerateMapData()
+    {
         //Allocate our map tiles
         tiles = new int[mapSizeX, mapSizeY];
         //Initialize our map tiles to be grass
-        for(int x = 0; x < mapSizeX; x++)
+        for (int x = 0; x < mapSizeX; x++)
         {
-            for(int y = 0; y < mapSizeY; y++)
+            for (int y = 0; y < mapSizeY; y++)
             {
                 tiles[x, y] = 0;
+            }
+        }
+
+        for (int x = 3; x <= 5; x++)
+        {
+            for (int y = 0; y < 4; y++)
+            {
+                tiles[x, y] = 1;
             }
         }
 
@@ -34,20 +48,33 @@ public class TileMap : MonoBehaviour
         tiles[4, 6] = 2;
         tiles[8, 5] = 2;
         tiles[8, 6] = 2;
-
-        //Now that we have our map data, let's spawn the visual prefabs
-        GenerateMapVisual();
     }
 
     private void GenerateMapVisual()
     {
-        for(int x = 0; x < mapSizeX; x++)
+        //Now that we have our map data, let's spawn the visual prefabs
+        for (int x = 0; x < mapSizeX; x++)
         {
             for(int y = 0; y < mapSizeY; y++)
             {
                 TileType tt = tileTypes[tiles[x, y]];
                 GameObject go = Instantiate(tt.tileVisualPrefab, new Vector3(x, y, 0), Quaternion.identity);
+
+                ClickableTile ct = go.GetComponent<ClickableTile>();
+                ct.tileX = x;
+                ct.tileY = y;
+                ct.map = this;
             }
         }
+    }
+    public Vector3 TileCoordToWorldCoord(int x, int y)
+    {
+        return new Vector3(x, y, 0);
+    }
+    public void MoveUnitTo(int x, int y)
+    {
+        selectedUnit.GetComponent<Unit>().tileX = x;
+        selectedUnit.GetComponent<Unit>().tileY = y;
+        selectedUnit.transform.position = TileCoordToWorldCoord(x, y);
     }
 }
